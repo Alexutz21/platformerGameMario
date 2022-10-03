e01080d1fc1bf128dc1f5759c1a728b74a0a4d6e
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-10-03 15:07:12 +0300

    move horizontally player
    we add new variable float runSpeed and [SerializeField]
    we add new variable Rigidbody2D myRigidbody
    in Start() { myRigidbody = GetComponent<Rigidbody2D>();
    we add new method Run() {
    Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
            myRigidBody.velocity = playerVelocity;
    }

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,19 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMove : MonoBehaviour
 {
     Vector2 moveInput;
-
+    [SerializeField] float runSpeed = 3f;
+    Rigidbody2D myRigidBody;
     void Start()
     {
-
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-
+        Run();
     }
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
 }
