adb4e10e1361475050f8859f6f72c93a6730f696
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-10-06 16:47:49 +0300

    enemy movement
    added c# script for enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D enemyRigidBody;
+    [SerializeField] float enemyMoveSpeed = 1f;
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        enemyRigidBody.velocity = new Vector2(enemyMoveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        enemyMoveSpeed = -enemyMoveSpeed;
+        FlipEnemyFacing();
+    }
+    void FlipEnemyFacing()
+    {
+        bool enemyHasHorizontalMove = Mathf.Abs(enemyRigidBody.velocity.x) > Mathf.Epsilon;
+        if (enemyHasHorizontalMove)
+        {
+            transform.localScale = new Vector2(-(Mathf.Sign(enemyRigidBody.velocity.x)), 1f);
+        }
+    }
+}
